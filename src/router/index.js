332bed822f61092aae25c46de58eeb175e131ed6
kimchulyeon <guinness987@gmail.com>
2022-06-05 22:52:32 +0900

    라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,51 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  // 로컬스토리지에 토큰 정보 유무 확인
+  const isAuth = localStorage.getItem("token");
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`;
+  // 로컬스토리지에 토큰이 있으면 페이지 이동, 없으면 로그인 페이지로 이동 (with 인코딩된 URI)
+  isAuth ? next() : next(loginPath);
+};
+
 const routes = [
   {
-    path: '/',
-    component: () => import('@/views/home/TheHome'),
+    path: "/",
+    component: () => import("@/views/home/TheHome"),
+    // Home으로 가기 전에 토큰 정보 확인
+    beforeEnter: requireAuth,
   },
   {
-    path: '/login',
-    component: () => import('@/views/login/LogIn'),
+    path: "/login",
+    component: () => import("@/views/login/LogIn"),
   },
   {
-    path: '/b/:bid',
-    component: () => import('@/views/board/BoardPage'),
+    path: "/b/:bid",
+    component: () => import("@/views/board/BoardPage"),
+    beforeEnter: requireAuth,
     children: [
       {
-        path: 'c/:cid',
-        component: () => import('@/components/card/TheCard'),
+        path: "c/:cid",
+        component: () => import("@/components/card/TheCard"),
+        beforeEnter: requireAuth,
       },
     ],
   },
   {
-    path: '*',
-    redirect: '/404',
+    path: "*",
+    redirect: "/404",
   },
   {
-    path: '/404',
-    component: () => import('@/views/pgNotFound/PageNotFound'),
+    path: "/404",
+    component: () => import("@/views/pgNotFound/PageNotFound"),
   },
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes,
 });
 

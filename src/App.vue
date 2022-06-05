55e11874cfaf4cab2dbc6d5e4b3ef9f914c89361
kimchulyeon <guinness987@gmail.com>
2022-06-05 22:51:04 +0900

    app 컴포넌트 스타일 추가

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div id="app">
     <TheHeader />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import TheHeader from '@/layouts/TheHeader.vue';
+import TheHeader from "@/layouts/TheHeader.vue";
 
 export default {
   components: {
@@ -14,3 +14,55 @@ export default {
   },
 };
 </script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  flex-grow: 1;
+  position: relative;
+}
+.btn {
+  border-radius: 3px;
+  padding: 6px 8px;
+  background-color: #e2e4e6;
+  border: none;
+  display: inline-block;
+  color: #fff;
+  font-size: 14px;
+  line-height: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.btn-success {
+  background-color: #5aac44;
+  box-shadow: 0 1px 0 #5aac44;
+}
+.form-control {
+  width: 100%;
+  box-sizing: border-box;
+  background-color: #e2e4e6;
+  border: 1px solid #cdd2d4;
+  border-radius: 3px;
+  display: block;
+  margin-bottom: 12px;
+  padding: 6px 8px;
+  transition: background-color 0.3s;
+}
+input[type="text"].form-control,
+input[type="password"].form-control,
+textarea.form-control {
+  font-size: 14px;
+}
+.form-control:focus {
+  background-color: #fff;
+}
+</style>
